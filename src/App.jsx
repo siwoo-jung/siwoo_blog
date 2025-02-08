190042c9b19d9e3a0f4917ff3054da94de3da329
Siwoo Jung <siwoojung@Siwoos-MacBook-Air.local>
2025-02-08 22:51:03 +1100

    testing route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
 
 const App = () => {
   return (
     <>
-      <Navbar></Navbar>
       <div>App</div>
+      <HashRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="about" element={<About />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 };
---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>This_is_about_page</div>;
+};
+
+export default About;
