30d06e1e9d3d86e570aa079b66d7fdfe63f91f94
Siwoo Jung <siwoojung@Siwoos-MacBook-Air.local>
2025-02-10 22:45:35 +1100

    Updates

---
+++ posts/1.jsx
@@ -0,0 +1,10 @@
+const Post1 = () => {
+  return (
+    <div>
+      <h1>Post 1</h1>
+      <p>Hi, this is my first post!</p>
+    </div>
+  );
+};
+
+export default Post1;
---
+++ posts/2.jsx
@@ -0,0 +1,8 @@
+const Post2 = () => (
+  <div>
+    <h1>Post 2</h1>
+    <div>Hello from Post 2</div>
+  </div>
+);
+
+export default Post2;
